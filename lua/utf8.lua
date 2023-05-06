debe03d6b9d514969a8dae3c7c6f4b780a2390ab
uga-rosa <uga6603@gmail.com>
2023-05-06 21:37:56 +0900

    remove vim.validate(); eliminate dependence on neovim and work independently

--- lua/utf8.lua
+++ lua/utf8.lua
@@ -7,6 +7,32 @@ local bor = bit.bor
 local rshift = bit.rshift
 local lshift = bit.lshift
 
+---@param fun_name string
+---@param arg_types { [1]: unknown, [2]: string, [3]: boolean? }[] Array of argument, its expected type and whether it is optional.
+---Usage:
+---
+--- ---@param s string
+--- ---@param i number
+--- ---@param j? number
+--- function foo(s, i, j)
+---   assertArgument("foo", {
+---     { s, "string" },
+---     { i, "number" },
+---     { j, "number", true },
+---   })
+---   -- do something
+--- end
+local function assertArgument(fun_name, arg_types)
+  local msg = "bad argument #%d to '%s' (%s expected, got %s)"
+  for i, v in ipairs(arg_types) do
+    local arg, expected_type, optional = v[1], v[2], v[3]
+    local got_type = type(arg)
+    if not (got_type == expected_type or optional and arg == nil) then
+      error(msg:format(i, fun_name, expected_type, got_type))
+    end
+  end
+end
+
 ---The pattern (a string, not a function) "[\0-\x7F\xC2-\xF4][\x80-\xBF]*",
 ---which matches exactly one UTF-8 byte sequence, assuming that the subject is a valid UTF-8 string.
 utf8.charpattern = "[%z\x01-\x7F\xC2-\xF4][\x80-\xBF]*"
@@ -18,7 +44,9 @@ utf8.charpattern = "[%z\x01-\x7F\xC2-\xF4][\x80-\xBF]*"
 function utf8.char(...)
   local buffer = {}
   for i, v in ipairs({ ... }) do
-    if v < 0 or v > 0x10FFFF then
+    if type(v) ~= "number" then
+      error(("bad argument #%d to 'char' (number expected, got %s)"):format(i, type(v)))
+    elseif v < 0 or v > 0x10FFFF then
       error(("bad argument #%d to 'char' (value out of range)"):format(i))
     elseif v < 0x80 then
       -- single-byte
@@ -121,8 +149,8 @@ end
 ---@param s string
 ---@return function iterator
 function utf8.codes(s)
-  vim.validate({
-    s = { s, "string" },
+  assertArgument("codes", {
+    { s, "string" },
   })
 
   local i = 1
@@ -161,11 +189,12 @@ end
 ---@param j? integer end position. default=i
 ---@return integer code point
 function utf8.codepoint(s, i, j)
-  vim.validate({
-    s = { s, "string" },
-    i = { i, "number", true },
-    j = { j, "number", true },
+  assertArgument("codepoint", {
+    { s, "string" },
+    { i, "number", true },
+    { j, "number", true },
   })
+
   i = normalize_range(s, i or 1)
   if i < 1 then
     error("bad argument #2 to 'codepoint' (out of bounds)")
@@ -217,11 +246,12 @@ end
 ---@return integer?
 ---@return integer?
 function utf8.len(s, i, j)
-  vim.validate({
-    s = { s, "string" },
-    i = { i, "number", true },
-    j = { j, "number", true },
+  assertArgument("len", {
+    { s, "string" },
+    { i, "number", true },
+    { j, "number", true },
   })
+
   i = normalize_range(s, i or 1)
   if i < 1 or #s + 1 < i then
     error("bad argument #2 to 'len' (initial position out of bounds)")
@@ -257,10 +287,10 @@ end
 ---@param i? integer start position. if n >= 0, default=1, otherwise default=#s+1
 ---@return integer?
 function utf8.offset(s, n, i)
-  vim.validate({
-    s = { s, "string" },
-    n = { n, "number" },
-    i = { i, "number", true },
+  assertArgument("offset", {
+    { s, "string" },
+    { n, "number" },
+    { i, "number", true },
   })
 
   i = i or (n >= 0 and 1 or #s + 1)
